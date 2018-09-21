e5bdeda9d157d1ac30b4ea061730cd661f39f7a3
github username <github email address>
2018-09-21 10:27:20 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,15 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+  create = document.getElementsByTagName("form")[0]
+  create.addEventListener("submit", a => {
+    a.preventDefault()
+    let b = document.getElementById("new-task-description")
+    let c = b.value
+    b.value = ""
+    let d = document.querySelector("#tasks")
+    let e = document.createElement("li")
+    e.innerText = c
+    d.appendChild(e)
+  })
 });
