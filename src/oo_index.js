5c1a40a50547603b056529392cfc0cba86cce97f
vavnilov <vadim.avnilov@gmail.com>
2018-09-21 17:22:37 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,25 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  // const taskList = new TaskList();
+});
+
+let tasks = document.querySelector('#tasks');
+let newTaskField = document.querySelector('#new-task-description');
+let submitButton = document.querySelectorAll('input')[1];
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    let task = document.createElement('li');
+    task.innerText = newTaskField.value;
+
+    let deleteButton = document.createElement('button');
+    deleteButton.innerText = "delete";
+    deleteButton.addEventListener('click', e => {
+      e.target.parentNode.remove();
+    })
+
+    task.appendChild(deleteButton)
+    tasks.appendChild(task);
+
+    newTaskField.value = '';
 });
