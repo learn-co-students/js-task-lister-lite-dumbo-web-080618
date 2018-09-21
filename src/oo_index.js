c16c6c0cdf0ac4d8ad38ba80a75e310fb5856789
mikepcarlos <michaelpcarlos@gmail.com>
2018-09-21 11:34:53 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,18 @@
+// let button = document.querySelector('input[type="submit"]');
+// let form = document.getElementById("create-task-form");
+let input = document.querySelector('input[type="text"]');
+let description = document.getElementById("new-task-description")
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
 });
+
+document.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // console.log(input.value)
+  let tasks = document.getElementById("tasks");
+  let li = document.createElement("li");
+  li.innerText = input.value;
+  tasks.appendChild(li);
+
+})
--- src/task.js
+++ src/task.js
@@ -1,2 +1,8 @@
 class Task {
+
+  // constructor(desc) {
+  //   this.text = desc;
+  // }
 }
+
+//Your task file is your model. It is responsible for creating tasks and assigning attributes to them.
