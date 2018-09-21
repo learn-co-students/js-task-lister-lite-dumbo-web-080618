3b2c137fd0964a5ec321b6e32fe2b63787807154
Flatiron School <flatironschool@Flatirons-MacBook-Air-4.local>
2018-09-21 11:12:01 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,22 @@
+// const newForm = document.getElementById("create-task-form")
+// const newDescription = document.getElementById("new-task-description")
+// const list = document.getElementById("tasks")
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+
+
+  // newForm.addEventListener("submit", (e)=> {
+  //   e.preventDefault(e)
+  //   let formData =  new FormData(newForm)
+  //   let description = formData.get("new-task-description")
+  //   let ordered =  document.createElement("li")
+  //   ordered.innerText =  `${description}`
+  //   list.append(ordered)
+
+
+
+
+
+  })
 });
