626fac11e061dc05a8b4df89f502ea642a30d1b4
linh4 <linh44h@gmail.com>
2018-09-21 12:41:45 -0400

    done

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,23 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+
+  const form = document.querySelector('#create-task-form');
+  const description = document.querySelector('#new-task-description');
+  const ul = document.querySelector('#tasks');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let li = document.createElement('li');
+    li.innerText = description.value;
+    description.value = '';
+    let btn = document.createElement('button');
+    btn.innerText = 'X';
+    btn.addEventListener('click', (e) => {
+      e.target.parentElement.remove();
+    })
+
+    li.appendChild(btn);
+    ul.appendChild(li);
+
+  })
 });
