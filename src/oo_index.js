0cfde276e80d7755deaf42d8980c519b6aba0bbc
github username <github email address>
2018-09-24 10:19:09 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -2,3 +2,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
 });
+
+const list = document.getElementById('list')
+const tasks = document.getElementById('tasks')
+
+
+function createToDo() {
+
+  let newToDo = document.getElementById("new-task-description").value
+
+  let newOne = new Task(newToDo);
+  newInfo = document.createElement('li')
+
+  newInfo.id = "added-task"
+  newInfo.dataset.num = newOne.id
+  newInfo.innerHTML = newOne.name + `<input type="button" value="X" onclick="remove(${newInfo.dataset.num})">`
+
+
+  list.appendChild(newInfo)
+}
+
+function remove(num){
+  let pending = Array.prototype.slice.call(document.querySelectorAll("#added-task"))
+
+  let goodbye = pending.filter(task => parseInt(task.dataset.num) === num)
+  goodbye[0].remove()
+
+}
--- src/task.js
+++ src/task.js
@@ -1,2 +1,11 @@
+let store = {tasks: []}
+let taskId = 0;
+
 class Task {
+  constructor(name){
+    this.name = name;
+    this.id = ++taskId
+    store.tasks.push(this)
+
+  }
 }
